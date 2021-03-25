9f34da867c9b8ace9bc87575f2728f5503d0fa72
B <alexandre.bomont@gmail.com>
2021-03-25 15:57:58 +0100

    responsive layout_no_sidebar.css, layout_sidebar.css, user_edit.css

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -28,33 +28,33 @@
     </header>
     <nav class="sidenav">
         <div class="sidebar_icon">
-            <img src="{{ Session::has('avatar') ? Session::get('avatar') : asset('/images/avatar.png') }}" alt="moi" class="avatar_sidebar">
+            <img src="{{ Session::has('avatar') ? Session::get('avatar') : asset('/images/avatar.png') }}" alt="moi" class="img_sidebar">
             <a class="personne" href="#"> @if(Session::has('LoggedUser')) {{ Session::get('LoggedUser') }}@else Moi @endif</a>
         </div>
 
         <div class="sideline"></div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/infos.png') }}" alt="informations">
+            <img src="{{ asset('/images/infos.png') }}" alt="informations" class="img_sidebar">
             <a href="{{ url('/user/edit') }}"> Mes informations</a>
         </div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/trajet.png') }}" alt="trajets">
+            <img src="{{ asset('/images/trajet.png') }}" alt="trajets" class="img_sidebar">
             <a href="#"> Mes trajets en attente</a>
         </div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/notification.png') }}" alt="notifications">
+            <img src="{{ asset('/images/notification.png') }}" alt="notifications" class="img_sidebar">
             <a href="#"> Mes notifications</a>
         </div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/groupes.png') }}" alt="groupes_amis">
+            <img src="{{ asset('/images/groupes.png') }}" alt="groupes_amis" class="img_sidebar">
             <a href="#"> Mes groupes d'amis</a>
         </div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/note.png') }}" alt="notes">
+            <img src="{{ asset('/images/note.png') }}" alt="notes" class="img_sidebar">
             <a href="#"> Mes notes attribuées</a>
         </div>
         <div class="sidebar_icon">
-            <img src="{{ asset('/images/deconnexion.png') }}" alt="déconnexion">
+            <img src="{{ asset('/images/deconnexion.png') }}" alt="déconnexion" class="img_sidebar">
             <a href="{{ url('/logout') }}"> Déconnexion</a>
         </div>
     </nav>
