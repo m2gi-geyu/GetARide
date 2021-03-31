5d5a7f6ad4e987c1d41a63f99f4d6b1e23946ea2
B <alexandre.bomont@gmail.com>
2021-03-31 18:11:26 +0100

    style email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+        <p class="title-logo">GET A RIDE</p>
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
