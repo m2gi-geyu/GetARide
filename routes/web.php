8b9f1d4b09730688e5c1ed8a11d3a43267e4447f
GE Yuxuan <gyx18971167768@gmail.com>
2021-04-07 22:52:58 +0200

    email verification version 3.1

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\TravelSearchController;
-use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\VerifyEmailController;
 use App\Http\Controllers\Security\askForPasswordReset;
 use App\Http\Controllers\Security\PasswordResetting;
 use App\Http\Controllers\RideController;
@@ -88,10 +87,9 @@ Route::get('email/verify', function () {
 })->name('verification.notice');
 
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/welcome');
-})->middleware( ['auth','signed'])->name('verification.verify');
+Route::get('/email/verify/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    ->middleware(['signed', 'throttle:6,1'])
+    ->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
