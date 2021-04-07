473dad8ac268722a13c259de4a17d071eb43d50c
Tung <tunghuong66bg@gmail.com>
2021-04-07 02:35:43 +0200

    ajouter IG de rechercher trajet

--- resources/views/trip/search_trip.blade.php
+++ resources/views/trip/search_trip.blade.php
@@ -1,25 +1,28 @@
 @extends('layouts.sidebar')
 <link href="{{ asset('/css/user_edit.css') }}" rel="stylesheet" type="text/css" >
+<link rel="stylesheet" href="{{asset('styles/bootstrap/dist/css/bootstrap.css')}}">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 @section('content')<div class="panel-body">
-    <h1 class="card-title">Search for a trip</h1>
+    <h1 class="card-title" style="color: #d6d8db;font-family: 'Agency FB'">Search for a trip</h1>
+
     <div>
-        <input type="text" name="start_town" id="start_town" class="form-control" placeholder="Ville de départ" onkeyup="fetch_trips()"/>
-        <input type="text" name="end_town" id="end_town" class="form-control" placeholder="Ville d'arrivée" onkeyup="fetch_trips()"/>
-        <input type="date" name="date" id="date" class="form-control" onchange="fetch_trips()"/>
+        <input style="margin: 2% 0" type="text" name="start_town" id="start_town" class="form-control" placeholder="Ville de départ" onkeyup="fetch_trips()"/>
+        <input style="margin: 2% 0" type="text" name="end_town" id="end_town" class="form-control" placeholder="Ville d'arrivée" onkeyup="fetch_trips()"/>
+        <input style="margin: 2% 0" type="date" name="date" id="date" class="form-control" onchange="fetch_trips()"/>
     </div>
     <div class="table-responsive">
-        <h3 align="center">Résultats trouvés : <span id="total_records"></span></h3>
+        <h2 align="center" style="color: #d6d8db;font-family: 'Agency FB'; margin:5% 0;">Résultats trouvés : <span id="total_records"></span></h2>
         <table class="table table-striped table-bordered">
-            <thead>
+            <thead style="font-family: 'Agency FB'; font-size: 1.2em;"; >
             <tr>
-                <th>Driver</th>
-                <th>Number of seats</th>
-                <th>Starting town</th>
-                <th>Ending town</th>
-                <th>Date of the trip</th>
-                <th>Price</th>
-                <th>Description</th>
+                <th style="color: #d6d8db">Driver</th>
+                <th style="color: #d6d8db">Number of seats</th>
+                <th style="color: #d6d8db">Starting town</th>
+                <th style="color: #d6d8db">Ending town</th>
+                <th style="color: #d6d8db">Date of the trip</th>
+                <th style="color: #d6d8db">Price</th>
+                <th style="color: #d6d8db">Description</th>
             </tr>
             </thead>
             <tbody>
@@ -27,12 +30,12 @@
             </tbody>
         </table>
     </div>
-    <div  ><a href="../dashboard"><button class="btn-dark btn" type="button">Revenir à l'accueil</button></a></div>
+    <div style="text-align: center;"><a href="../dashboard"><button class="btn-dark btn" type="button" style="font-family:'Agency FB'; font-size: 1.6em";>Revenir à l'accueil</button></a></div>
 </div>
 
 
 
-
+<div style="background-color: coral;">
 <script>
 
     $(document).ready(function(){
@@ -52,10 +55,13 @@
             dataType:'json',
             success:function(data)
             {
-                $('tbody').html(data.table_data);
-                $('#total_records').text(data.total_data);
+                $('tbody').html(data.table_data).css({"font-family": "Agency FB", "font-size": "1.2em", "color":"#43de86", "hover":"yellow"});
+                $('#total_records').text(data.total_data).css({"font-family": "Agency FB"});
             }
         })
+       
     }
 </script>
+
+</div>
 @endsection
