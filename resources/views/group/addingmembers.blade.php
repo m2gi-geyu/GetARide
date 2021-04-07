36c1a6e0732edbb2c3e0f5140c3a65618b1d742f
Walid <neo.purpinc@gmail.com>
2021-04-07 12:55:28 +0200

    [Create_Group] MàJ CSS

--- resources/views/group/addingmembers.blade.php
+++ resources/views/group/addingmembers.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.sidebar')
-<link href="{{ asset('/css/user_edit.css') }}" rel="stylesheet" type="text/css" >
 <link rel="stylesheet" href="{{asset('styles/bootstrap/dist/css/bootstrap.css')}}">
+<link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css" >
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-@section('content')<div class="panel-body">
-    <h1 class="card-title" style="color: #d6d8db;font-family: 'Agency FB'"> Ajoutez des membres à votre nouveau groupe!</h1>
-    <div class="form-group">
-        <input type="text" name="search" id="search" class="form-control" placeholder="Rechercher un utilisateur" />
-    </div>
-    <div class="table-responsive">
-        <h3 align="center" style="color: #d6d8db;font-family: 'Agency FB'; margin:5% 0;">Résultats trouvés : <span id="total_records"></span></h3>
-        <table class="table table-striped table-bordered">
-            <thead>
-            <tr>
-                <th style="color: #d6d8db">Pseudo</th>
-                <th style="color: #d6d8db">Ajouter au groupe</th>
-            </tr>
-            </thead>
-            <tbody>
+@section('content')
+    <div class="panel-body container">
+        <header class="header" >
+            <h2 id="colored_header" align="center">Ajoutez des membres à votre nouveau groupe!</h2>
+        </header>
+        <div class="form-group container" align="center">
+            <input style="width:50%" type="text" name="search" id="search" class="form-control" placeholder="Rechercher un utilisateur" /><br>
+            <h4 align="center">Résultats trouvés : <span id="total_records"></span></h4>
+        </div>
+        <div class="table-responsive container" id="table_group_add_user_div">
+            <table class="table table-striped table-bordered" id="table_group_add_user">
+                <thead>
+                    <tr>
+                        <th style="color: #d6d8db">Pseudo</th>
+                    </tr>
+                </thead>
+                <tbody>
 
-            </tbody>
-        </table>
-    </div>
-    <div style="text-align: center;"><a href="../dashboard"><button class="btn-dark btn" type="button" style="font-family:'Agency FB'; font-size: 1.6em";>Revenir à l'accueil</button></a></div>
+                </tbody>
+            </table>
+        </div>
+        <div class="container" align="center">
+            <a href="../dashboard"><button class="btn-perso" type="button" >Revenir à l'accueil</button></a>
+        </div>
 </div>
 
 
@@ -42,8 +46,9 @@
                 dataType:'json',
                 success:function(data)
                 {
-                    $('tbody').html(data.table_data).css({"font-family": "Agency FB", "font-size": "1.2em", "color":"#43de86", "hover":"yellow", "text-align":"center"});
+                    $('tbody').html(data.table_data);
                     $('#total_records').text(data.total_data);
+                    $('#table_group_add_user button').addClass('btn-perso-small');
                 }
             })
         }
--- resources/views/group/creategroup.blade.php
+++ resources/views/group/creategroup.blade.php
@@ -1,41 +1,28 @@
 @extends('layouts.sidebar')
-<link href="{{ asset('/css/user_edit.css') }}" rel="stylesheet" type="text/css" >
 <link rel="stylesheet" href="{{asset('styles/bootstrap/dist/css/bootstrap.css')}}">
+<link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css" >
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 @section('content')
-    <div class="container ">
+    <div class="panel-body container">
+        <header class="header" >
+            <h2 id="colored_header" align="center">Créez votre groupe d'amis !</h2>
+        </header>
+        <div class="form-group container" align="center">
+            <form action="{{ route('group/create') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <fieldset >
+                    <div class="form-group" >
+                        <h2 style="font-family:'Agency FB';font-size: 3em " for="group_name" >Nom du groupe</h2>
+                        <input style="width:60%" type="text" class="form-control input-modif" name="group_name"  placeholder="EKIPAFOND"  required value="{{old('group_name')}}">
+                        <span class="text-danger">@error('group_name'){{$message}}@enderror</span>
+                    </div>
+                </fieldset>
 
-        <div class="row">
-            <div class="col-md-3"></div>
-
-            <div class="col-md-3" style="text-align: center;">
-                <div class="form-group" >
-                    <form  class="col-md-8" action="{{ route('group/create') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <fieldset >
-                            <div class="form-group" >
-                                <h2 style="font-family:'Agency FB';font-size: 3em " for="group_name" >Nom du groupe</h2>
-                                <input type="text" class="form-control input-modif" name="group_name"  placeholder="EKIPAFOND"  required value="{{old('group_name')}}">
-                                <span class="text-danger">@error('group_name'){{$message}}@enderror</span>
-                            </div>
-                        </fieldset>
-
-                        <div class="form-group">
-                            <button style="font-family:'Agency FB';font-size: 1.0em" type="submit" class="btn-form " >Créer le groupe</button>
-                        </div>
-                    </form>
+                <div class="form-group">
+                    <button style="font-family:'Agency FB';font-size: 1.0em" type="submit" class="btn-perso" >Créer le groupe</button>
                 </div>
-
-
-
-            </div>
-
-            <div class="col-md-3"></div>
-            <div class="col-md-3"></div>
-
+            </form>
         </div>
     </div>
-
-
 @endsection
