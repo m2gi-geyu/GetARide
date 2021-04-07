de3a866bfb1815194aad36d3f7cde59504b65066
Walid <neo.purpinc@gmail.com>
2021-04-07 12:15:57 +0200

    [Search_Trip] MàJ CSS

--- resources/views/trip/search_trip.blade.php
+++ resources/views/trip/search_trip.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.sidebar')
-<link href="{{ asset('/css/user_edit.css') }}" rel="stylesheet" type="text/css" >
+
 <link rel="stylesheet" href="{{asset('styles/bootstrap/dist/css/bootstrap.css')}}">
+<link href="{{ asset('/css/welcome.css') }}" rel="stylesheet" type="text/css" >
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-@section('content')<div class="panel-body">
-    <h1 class="card-title" style="color: #d6d8db;font-family: 'Agency FB'">Search for a trip</h1>
-
-    <div style="text-align: center;">
-        <input style="margin: 2% 0" type="text" name="start_town" id="start_town" class="form-control" placeholder="Ville de départ" onkeyup="fetch_trips()"/>
-        <input style="margin: 2% 0" type="text" name="end_town" id="end_town" class="form-control" placeholder="Ville d'arrivée" onkeyup="fetch_trips()"/>
-        <input style="margin: 2% 0" type="date" name="date" id="date" class="form-control" onchange="fetch_trips()"/>
+@section('content')
+    <div class="panel-body container">
+    <header class="header" >
+        <h2 id="colored_header" align="center">Recherche de trajet</h2>
+    </header>
+    <div class="form-group container" align="center">
+        <input style="width: 50%; margin-bottom: 5px" type="text" name="start_town" id="start_town" class="form-control" placeholder="Ville de départ" onkeyup="fetch_trips()"/>
+        <input style="width: 50%; margin-bottom: 5px" type="text" name="end_town" id="end_town" class="form-control" placeholder="Ville d'arrivée" onkeyup="fetch_trips()"/>
+        <input style="width: 50%" type="date" name="date" id="date" class="form-control" onchange="fetch_trips()"/><br>
+        <h4 align="center">Résultats trouvés : <span id="total_records"></span></h4>
     </div>
-    <div class="table-responsive">
-        <h2 align="center" style="color: #d6d8db;font-family: 'Agency FB'; margin:5% 0;">Résultats trouvés : <span id="total_records"></span></h2>
-        <table class="table table-striped table-bordered">
+    <div class="table-responsive container" id="table_trip_find_div">
+        <table class="table table-striped table-bordered" id="table_trip_find">
             <thead style="font-family: 'Agency FB'; font-size: 1.2em;"; >
             <tr>
                 <th style="color: #d6d8db">Driver</th>
@@ -30,7 +33,7 @@
             </tbody>
         </table>
     </div>
-    <div style="text-align: center;"><a href="../dashboard"><button class="btn-dark btn" type="button" style="font-family:'Agency FB'; font-size: 1.6em";>Revenir à l'accueil</button></a></div>
+    <div style="text-align: center;"><a href="../dashboard"><button class="btn-perso" type="button" style="font-family:'Agency FB'; font-size: 1.6em";>Revenir à l'accueil</button></a></div>
 </div>
 
 
@@ -55,11 +58,11 @@
             dataType:'json',
             success:function(data)
             {
-                $('tbody').html(data.table_data).css({"font-family": "Agency FB", "font-size": "1.2em", "color":"#43de86", "hover":"yellow", "text-align":"center"});
-                $('#total_records').text(data.total_data).css({"font-family": "Agency FB"});
+                $('tbody').html(data.table_data);
+                $('#total_records').text(data.total_data);
             }
         })
-       
+
     }
 </script>
 
