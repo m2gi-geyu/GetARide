e3f4f232a7353f955cd989586902a8b0a2fa7e5c
Walid <neo.purpinc@gmail.com>
2021-04-08 18:54:17 +0200

    [ConsulterProfil] FRONT FINISH

--- resources/views/user/check_user_profile.blade.php
+++ resources/views/user/check_user_profile.blade.php
@@ -4,30 +4,23 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 @section('content')
-    <div class="panel-body container" align="center" >
-        <div class="container">
-            <h4 style="font-size: 50px; color: #f4a261">{{$user->username}}</h4>
-        </div>
-        <img src="{{ $user->profile_pic ? asset('storage/'.$user->username.'/'.$user->profile_pic) : asset('/images/avatar_gros.png') }}" id="output" class="resize" alt="avatar">
-        <div class="container">
-            @if($user->ratings==null)
-                <h3>Pas encore d'évaluation</h3>
-            @else
-                <div class="" >
-                    <h3>Note:{{$user->ratings}}</h3>
-                </div>
-            @endif
-        </div>
-        <div class="container">
-            <a href="../search"><button class="btn-perso" type="button">Moteur de recherche</button></a>
-        </div>
+<div class="panel-body container" align="center" >
+    <div class="container">
+        <h4 style="font-size: 50px; color: #f4a261">{{$user->username}}</h4>
     </div>
+    <img src="{{ $user->profile_pic ? asset('storage/'.$user->username.'/'.$user->profile_pic) : asset('/images/avatar_gros.png') }}" id="output" class="resize" alt="avatar">
+    <div class="container">
+        @if($user->ratings==null)
+            <h4>Pas encore d'évaluation</h4>
+        @else
+            <h4>Note:{{$user->ratings}}</h4>
+        @endif
     </div>
-
-    <div class="pour-walid">
-        <h1>Trajets crées</h1>
-
-        <table class="table table-striped table-bordered">
+    <header class="header">
+        <h3 align="center">Ses trajets</h3>
+    </header>
+    <div style="width:80%" class="table-responsive container" id="ses_trajets_div">
+        <table class="table table-striped table-bordered" id="ses_trajets">
             <thead>
             <tr>
                 <th style="color: #d6d8db">Nombre de sièges</th>
@@ -42,18 +35,23 @@
             <tbody>
             @foreach($created_trips as $trip)
                 <tr>
-                    <td style="color: #d6d8db">{{$trip->number_of_seats}}</td>
+                    <td class="first_cell" style="color: #d6d8db">{{$trip->number_of_seats}}</td>
                     <td style="color: #d6d8db">{{$trip->starting_town}}</td>
                     <td style="color: #d6d8db">{{$trip->ending_town}}</td>
                     <td style="color: #d6d8db">{{$trip->date_trip}}</td>
                     <td style="color: #d6d8db">{{$trip->price}}</td>
                     <td style="color: #d6d8db">{{$trip->description}}</td>
-                    <td style="color: #d6d8db">{{$trip->precision}}</td>
+                    <td class="before_last_cell" style="color: #d6d8db">{{$trip->precision}}</td>
                 </tr>
             @endforeach
             </tbody>
-
         </table>
     </div>
+    <div class="container">
+        <a href="../search"><button class="btn-perso" type="button">Moteur de recherche</button></a>
+    </div>
+</div>
+</div>
+
 
 @endsection
