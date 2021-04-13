2e779daf11f530bfcc59a95c6d93cf7ac3d28f8c
GE Yuxuan <gyx18971167768@gmail.com>
2021-04-13 21:33:32 +0200

    email verification version 7.3

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
         <div class="mb-4 text-sm text-gray-600" style="color:red">
-            <h3>Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.</h3>
+            <h3>Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.</h3>
         </div>
 
         @if (session('status') == 'verification-link-sent')
