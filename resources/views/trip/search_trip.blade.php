3eb077619e04b2050f656c98b3231c68f5b4dc11
Walid <neo.purpinc@gmail.com>
2021-04-13 11:29:18 +0200

    [SearchTrip] MàJ FRONT suite à l'ajout du choix public (pour Guizmo)

--- resources/views/trip/search_trip.blade.php
+++ resources/views/trip/search_trip.blade.php
@@ -13,7 +13,14 @@
         <input style="width: 50%; margin-bottom: 5px" type="text" name="start_town" id="start_town" class="form-control" placeholder="Ville de départ" onkeyup="fetch_trips()"/>
         <input style="width: 50%; margin-bottom: 5px" type="text" name="end_town" id="end_town" class="form-control" placeholder="Ville d'arrivée" onkeyup="fetch_trips()"/>
         <input style="width: 50%" type="date" name="date" id="date" class="form-control" onchange="fetch_trips()"/>
-        Trajet privé ?<input type="checkbox" name="chkBox" id="chkBox" class="form-control" onchange="fetch_trips()"/><br/>
+        <div class="btn-group btn-group-toggle" style="background:#e9c46a; padding: 5px; margin-top: 5px;border:2px solid #e76f51;border-radius: 15px" data-toggle="buttons" onchange="fetch_trips()">
+            <label class="btn btn-secondary active">
+                <input type="radio" name="chkBox" id="public_option" autocomplete="off" checked> Public
+            </label>
+            <label class="btn btn-secondary">
+                <input type="radio" name="chkBox" id="private_option" autocomplete="off"> Privé
+            </label>
+        </div>
         <h4 align="center">Résultats trouvés : <span id="total_records"></span></h4>
     </div>
     <div class="table-responsive container" id="table_trip_find_div">
