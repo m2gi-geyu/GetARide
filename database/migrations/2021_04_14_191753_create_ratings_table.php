3a82e181f0bc4a4867a0cbc010fceea8e2a7dbcf
schmit62u <schmit62u@etu.univ-lorraine.fr>
2021-04-14 20:35:16 +0200

    Ajout de la création de table de notations

---
+++ database/migrations/2021_04_14_191753_create_ratings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->integer('id_rater');
+            $table->foreign('id_rater')->references('id')->on('users');
+            $table->integer('id_rated');
+            $table->foreign('id_rated')->references('id')->on('users');
+            $table->integer('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
